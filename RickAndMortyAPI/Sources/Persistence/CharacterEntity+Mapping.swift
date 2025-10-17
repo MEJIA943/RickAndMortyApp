8ca55f45bb74ee3d47a9d89fc1f37fd1fe74713a
Eduardo Mejia <lalo.ejmv@gmail.com>
2025-10-17 16:52:39 -0600

    feat: initial core data

--- RickAndMortyAPI/Sources/Persistence/CharacterEntity+Mapping.swift
+++ RickAndMortyAPI/Sources/Persistence/CharacterEntity+Mapping.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import CoreData
+
+@objc(CharacterEntity)
+public class CharacterEntity: NSManagedObject {
+
+}
+
+extension CharacterEntity {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CharacterEntity> {
+        return NSFetchRequest<CharacterEntity>(entityName: "CharacterEntity")
+    }
+
+    @NSManaged public var id: String?
+    @NSManaged public var name: String?
+    @NSManaged public var species: String?
+    @NSManaged public var status: String?
+    @NSManaged public var gender: String?
+    @NSManaged public var image: String?
+    @NSManaged public var episodesViewed: [String]?
+    @NSManaged public var isFavorite: Bool
+}
+
+
+
--- RickAndMortyAPI/Sources/Persistence/CoreDataStack.swift
+++ RickAndMortyAPI/Sources/Persistence/CoreDataStack.swift
@@ -5,4 +5,38 @@
 //  Created by Eduardo Javier Mejia Valdiviezo on 17/10/25.
 //
 
-import Foundation
+import CoreData
+
+/// Stack de CoreData para la app
+final class CoreDataStack {
+    static let shared = CoreDataStack()
+    
+    // Contexto principal para UI y repositorios
+    lazy var context: NSManagedObjectContext = persistentContainer.viewContext
+    
+    private init() {}
+    
+    // Contenedor persistente
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "RickAndMortyModel") // debe coincidir con tu .xcdatamodeld
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error {
+                fatalError("No se pudo cargar CoreData: \(error)")
+            }
+        }
+        return container
+    }()
+    
+    // Guardar cambios
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                print("Error guardando CoreData: \(error)")
+            }
+        }
+    }
+}
+
